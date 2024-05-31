e15e66cf56be2b12c66be2a06bc53f55a9fa85e5
Dipen Patel <dipen.ptl1@gmail.com>
2024-06-01 01:22:57 +0300

    Using delegate & protocol to pass data from child view to viewcontroller

---
+++ nested-scroll-ios/ViewController/SongDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SongDetailViewController.swift
+//  nested-scroll-ios
+//
+//  Created by Dipen Patel on 01/06/24.
+//
+
+import UIKit
+
+class SongDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ nested-scroll-ios/ViewModel/SongDetailViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SongDetailViewModel.swift
+//  nested-scroll-ios
+//
+//  Created by Dipen Patel on 01/06/24.
+//
+
+import Foundation
