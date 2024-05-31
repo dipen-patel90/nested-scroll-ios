22101b2489b910303b0ffa2dfa6ac5b709dc86c3
Dipen Patel <dipen.ptl1@gmail.com>
2024-05-31 22:43:28 +0300

    Initial commit

---
+++ nested-scroll-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  nested-scroll-ios
+//
+//  Created by Dipen Patel on 31/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
